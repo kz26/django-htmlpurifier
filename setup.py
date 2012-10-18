a6439d3f2eb75adb270e97026d1a9d46881fcc7a
Tarrant <tarrant@twitch.tv>
2012-10-18 15:42:21 -0700

    Make htmlpurifier installable via pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup, Extension
+
+
+setup ( name = 'htmlpurifier',
+        version = open('lib/__version__.py').readline().rstrip().split('=')[1].strip('"'),
+        author = 'whitehat2k9',
+        author_email = '',
+        description = 'A silly but useful hack for sanitizing untrusted HTML input in Django forms via the PHP HTML Purifier library',
+        package_dir = {'htmlpurifier': 'lib'},
+        packages = ['htmlpurifier'],
+       )
