018bcffb94e77645c79346f55b71b980c6541242
kz26 <whitehat2k9@gmail.com>
2013-11-16 17:23:25 -0600

    Modify author name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup, Extension
 
 setup ( name = 'htmlpurifier',
         version = open('lib/__version__.py').readline().rstrip().split('=')[1].strip('"'),
-        author = 'whitehat2k9',
+        author = 'kz26',
         author_email = '',
         description = 'A silly but useful hack for sanitizing untrusted HTML input in Django forms via the PHP HTML Purifier library',
         package_dir = {'htmlpurifier': 'lib'},
